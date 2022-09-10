acbe8aba3aa27b7e2cf7801c4cc6e13fd187f621
fuwafuwa1121 <fuwafuwa.public1121@gmail.com>
2022-09-10 15:07:06 +0900

    API接続しjsonデータの取り出しに成功

---
+++ src/apiKey.js
@@ -0,0 +1,3 @@
+export default function apiKey() {
+    return "8zcmxWVVsrCbvLmCUwL65PIYWdmDzxeGzMSrVj0d";
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
+import apiKey from "./apiKey";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Population extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: Array(47).fill(false),
+            prefectures: {},
+        };
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    componentDidMount() {
+        fetch(
+            "https://opendata.resas-portal.go.jp/api/v1/population/sum/perYear?cityCode=11362&prefCode=11",
+            {
+                headers: { "X-API-KEY": apiKey() },
+            }
+        )
+            .then((responce) => responce.json())
+            .then((res) => {
+                this.setState({ prefectures: res.result });
+            });
+    }
+
+    render() {
+        let { selected, prefectures } = this.state;
+        console.log(prefectures);
+        return (
+            <>
+                <p>成功</p>
+            </>
+        );
+    }
+}
+
+export default Population;
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Population />);
